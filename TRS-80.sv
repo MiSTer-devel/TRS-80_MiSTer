b44d863ff969d534994cc757f109b5ddf14fabda
Brendan Saricks <TheJesusFish@users.noreply.github.com>
2025-09-08 01:45:29 -0500

    Sys update and 20250907 release (#48)

--- TRS-80.sv
+++ TRS-80.sv
@@ -48,6 +48,8 @@ module emu
 	input  [11:0] HDMI_WIDTH,
 	input  [11:0] HDMI_HEIGHT,
 	output        HDMI_FREEZE,
+	output        HDMI_BLACKOUT,
+	output        HDMI_BOB_DEINT,
 
 `ifdef MISTER_FB
 	// Use framebuffer in DDRAM
@@ -167,6 +169,8 @@ module emu
 
 assign VGA_F1=0;
 assign HDMI_FREEZE = 0;
+assign HDMI_BLACKOUT = 0;
+assign HDMI_BOB_DEINT = 0;
 assign VGA_SCALER  = 0;
 assign VGA_DISABLE = 0;
 
